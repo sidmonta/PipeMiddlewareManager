0a6eb18d5f736b24e46bdf7b79514b2af108c7fa
Sid monta <luca.montanera@vlc2.com>
2025-11-18 22:23:46 +0100

    chore(ts): add TypeScript config, update rollup for typescript, add dev deps and scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,22 @@
-// import babel from "@rollup/plugin-babel"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "rollup-plugin-commonjs"
+import typescript from "@rollup/plugin-typescript"
 import { terser } from "rollup-plugin-terser"
 
 export default {
   input: "lib/index.js",
   output: [
     { file: "build/bundle.es.js", format: "es", sourcemap: false },
-    {
-      file: "build/bundle.js",
-      format: "umd",
-      sourcemap: false,
-      name: "pipe-manager",
-    },
+    { file: "build/bundle.js", format: "umd", sourcemap: false, name: "pipe-manager" }
   ],
   plugins: [
     commonjs(),
     resolve({
       customResolveOptions: {
-        moduleDirectories: ["node_modules"],
-      },
+        moduleDirectories: ["node_modules"]
+      }
     }),
-    // babel({ babelHelpers: "runtime", exclude: "**/node_modules/**" }),
-    terser(),
-  ],
+    typescript({ tsconfig: "./tsconfig.json" }),
+    terser()
+  ]
 }
