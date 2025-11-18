95dbebcf7a5c0f8f3f48da31d1ee018526911365
Sid monta <luca.montanera@vlc2.com>
2025-11-18 22:50:52 +0100

    chore(ts): tighten types in lib/common-middleware.ts (replace any with generics/tuples)

--- lib/common-middleware.ts
+++ lib/common-middleware.ts
@@ -1,33 +1,41 @@
-const generateMessage = (
-  message: string | ((...args: any[]) => string),
-  params: any[]
-): string =>
-  typeof message === "function" ? message(...params) : message
+import { StopCall, MaybePromise, Middleware } from './types'
 
-export function throwError(errorMessage: string | ((...args: any[]) => string)) {
-  return () => (...params) => {
+const generateMessage = (
+  message: string | ((...args: unknown[]) => string),
+  params: unknown[]
+): string => (typeof message === 'function' ? (message as (...args: unknown[]) => string)(...params) : message)
+
+export function throwError<In = unknown, Rest extends unknown[] = unknown[]>(
+  errorMessage: string | ((...args: [In, ...Rest]) => string)
+): Middleware<unknown, In, never, Rest> {
+  return () => (...params: [In, ...Rest]) => {
     throw new Error(generateMessage(errorMessage, params))
   }
 }
 
-export function stop(stopMessage: string | ((...args: any[]) => string)) {
-  return () => (...params) => {
+export function stop<In = unknown, Rest extends unknown[] = unknown[]>(
+  stopMessage: string | ((...args: [In, ...Rest]) => string)
+): Middleware<unknown, In, never, Rest> {
+  return () => (...params: [In, ...Rest]) => {
     throw new StopCall(generateMessage(stopMessage, params))
   }
 }
-}
 
-export function pushData<T>(data: T) {
+export function pushData<T>(data: T): Middleware<unknown, unknown, T, unknown[]> {
   return () => () => data
 }
 
-export function tap(fun: (...args: any[]) => any | Promise<any>) {
-  return (deps) => async (...params) => {
+export function tap<In = unknown, Rest extends unknown[] = unknown[]>(
+  fun: (...args: [In, ...Rest]) => MaybePromise<unknown>
+): Middleware<unknown, In, In, Rest> {
+  return () => async (...params: [In, ...Rest]) => {
     await fun(...params)
     return params[0]
   }
 }
 
-export function map(fun: (...args: any[]) => any | Promise<any>) {
-  return (deps) => async (...params) => await fun(...params)
+export function map<In = unknown, Out = unknown, Rest extends unknown[] = unknown[]>(
+  fun: (...args: [In, ...Rest]) => MaybePromise<Out>
+): Middleware<unknown, In, Out, Rest> {
+  return () => async (...params: [In, ...Rest]) => await fun(...params)
 }
